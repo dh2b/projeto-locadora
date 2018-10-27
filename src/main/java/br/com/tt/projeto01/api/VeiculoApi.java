5ed0d6cfd70e88dc895cdb3870ea095d3f5eb40b
Darcio Bertocco <darciohbb@gmail.com>
2018-10-26 22:21:17 -0300

    Adiciona classes para API

--- src/main/java/br/com/tt/projeto01/api/VeiculoApi.java
+++ src/main/java/br/com/tt/projeto01/api/VeiculoApi.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tt.projeto01.model.Veiculo;
 import br.com.tt.projeto01.repository.VeiculoRepository;
 
 @RestController
+@RequestMapping("/api/veiculos")
 public class VeiculoApi {
-
 	@Autowired
-	private VeiculoRepository repo;
+	VeiculoRepository repo;
 
-	@GetMapping ("/api/veiculos")
+	@GetMapping
 	public List<Veiculo> veiculos() {
 		return repo.findAll();
 
+	}
+
+	@PostMapping
+	public void salvar(@RequestBody Veiculo veiculo) {
+		repo.save(veiculo);
 	}
 }
--- src/main/java/br/com/tt/projeto01/controller/ClienteController.java
+++ src/main/java/br/com/tt/projeto01/controller/ClienteController.java
@@ -1,11 +1,18 @@
 package br.com.tt.projeto01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/cliente")
 public class ClienteController {
-
+	
+	private final String template1 = "cliente"; 
+	
+	
 	@GetMapping("/cliente")
 	public String cliente() {
 		return "/cliente";
